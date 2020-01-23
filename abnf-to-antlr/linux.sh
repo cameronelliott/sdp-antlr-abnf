0144d3138ecf42687b889b35aca6db86aeb0f467
Cameron Elliott <cameron-elliott@users.noreply.github.com>
2020-01-22 21:10:16 -0800

    update antlr from 4.7.2 to 4.8

--- abnf-to-antlr/linux.sh
+++ abnf-to-antlr/linux.sh
@@ -29,12 +29,12 @@ unix2dos sdp.abnf           # fixup crlf
 # create a wellformed g4 file, with header line
 echo "grammar sdp;" >sdp.g4
 mono ./Abnf-To-Antlr/AbnfToAntlr/bin/Debug/AbnfToAntlr.exe sdp.abnf >>sdp.g4
-java -jar antlr/antlr-4.7.2-complete.jar -Dlanguage=Go -o ../output-go sdp.g4
-java -jar antlr/antlr-4.7.2-complete.jar -Dlanguage=Java -o ../output-java sdp.g4
-java -jar antlr/antlr-4.7.2-complete.jar -Dlanguage=CSharp -o ../output-csharp sdp.g4
-java -jar antlr/antlr-4.7.2-complete.jar -Dlanguage=Cpp -o ../output-cpp sdp.g4
-java -jar antlr/antlr-4.7.2-complete.jar -Dlanguage=Javascript -o ../output-js sdp.g4
-java -jar antlr/antlr-4.7.2-complete.jar -Dlanguage=Python -o ../output-python sdp.g4
-java -jar antlr/antlr-4.7.2-complete.jar -Dlanguage=Swift -o ../output-swift sdp.g4
-java -jar antlr/antlr-4.7.2-complete.jar -Dlanguage=PHP -o ../output-php sdp.g4
+java -jar antlr/antlr-4.8-complete.jar -Dlanguage=Go -o ../output-go sdp.g4
+java -jar antlr/antlr-4.8-complete.jar -Dlanguage=Java -o ../output-java sdp.g4
+java -jar antlr/antlr-4.8-complete.jar -Dlanguage=CSharp -o ../output-csharp sdp.g4
+java -jar antlr/antlr-4.8-complete.jar -Dlanguage=Cpp -o ../output-cpp sdp.g4
+java -jar antlr/antlr-4.8-complete.jar -Dlanguage=Javascript -o ../output-js sdp.g4
+java -jar antlr/antlr-4.8-complete.jar -Dlanguage=Python -o ../output-python sdp.g4
+java -jar antlr/antlr-4.8-complete.jar -Dlanguage=Swift -o ../output-swift sdp.g4
+java -jar antlr/antlr-4.8-complete.jar -Dlanguage=PHP -o ../output-php sdp.g4
 
